6299e3cc22884276baae10a7f3fb0a15fb45ca37
MayankTravel <mayank.kumar@midastravel.org>
2023-05-20 03:17:29 +0530

    URL Changes

--- src/static.js
+++ src/static.js
@@ -1,5 +1,5 @@
 // export const host = "https://asfiya-art-shop-backend.vercel.app/";
 // export const host = "http://192.168.3.116:9000/";
 // export const host = "http://192.168.29.91:9000/";
-export const host = "http://65.1.114.225:9000/";
+export const host = "https://api.apisecommerce.com/";
 // export const host = "http://192.168.3.116:9000/";
