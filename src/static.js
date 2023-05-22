008f48fb4b4caa78aee2b8e88469fe7d1986c127
MayankTravel <mayank.kumar@midastravel.org>
2023-05-22 18:58:18 +0530

    Typo Error Resolved

--- src/static.js
+++ src/static.js
@@ -1,5 +1,5 @@
 // export const host = "https://asfiya-art-shop-backend.vercel.app/";
 // export const host = "http://192.168.3.116:9000/";
 // export const host = "http://192.168.29.91:9000/";
-export const host = "https://asiya.apisecommerce.com/";
+export const host = "https://asfiya.apisecommerce.com/";
 // export const host = "http://192.168.3.116:9000/";
