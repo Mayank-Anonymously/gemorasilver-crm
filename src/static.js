0edbedab8f87be3a5afb98c37d2133dfeb1f50de
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-10-05 11:56:49 +0530

    added changes

--- src/static.js
+++ src/static.js
@@ -1,5 +1,2 @@
-// export const host = "https://asfiya-art-shop-backend.vercel.app/";
-// export const host = "http://192.168.3.116:9000/";
-// export const host = "http://192.168.29.91:9000/";
 export const host = 'https://api.lunivajewels.com/';
 // export const host = 'http://192.168.1.2:9000/';
